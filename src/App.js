ce96b1a7410f394492cab1628f61eca407b999c8
Jesse Kämäräinen <jesse@bromeco.fi>
2021-11-27 13:09:02 +0200

    finish app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+   const [text, setText] = useState('')
+
+   async function copy() {
+         /* Get the text field */
+         var copyText = document.getElementById("htmlText")
+
+         /* Select the text field */
+         // copyText.select()
+         // copyText.setSelectionRange(0, 99999) /* For mobile devices */
+
+          /* Copy the text inside the text field */
+         await navigator.clipboard.writeText(copyText.innerText)
+
+         /* Alert the copied text */
+         console.log("Copied the text: " + copyText.innerText)
+   }
+
+   return (
+      <div className="App">
+         <h3>Text to html tool</h3>
+         <div style={{ maxWidth: 500 }}>
+            <ReactQuill value={text} onChange={setText} style={{ height: 200, marginBottom: 60 }} />
+            <p id="htmlText">{text.toString()}</p>
+         </div>
+         <button onClick={copy}>Copy</button>
+      </div>
+   )
 }
 
 export default App;
